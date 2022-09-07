d6fc5b44ac6efae6b85ef5c9dd87df46ce5a0874
Mikegit config --global user.email mikmercieca13@gmail.com <101655537+Mike-Mercieca@users.noreply.github.com>
2022-09-07 10:31:31 +0100

    add testing for app.js

---
+++ test/app.test.js
@@ -0,0 +1,35 @@
+const supertest = require('supertest');
+const app = require('../api/app');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+describe('App routes', () => {
+    let api;
+
+    beforeAll(async () => {
+        await mongoose
+            .connect(process.env.MONGO_URI, {
+                useNewUrlParser: true,
+                useCreateIndex: true,
+                useFindAndModify: false,
+                useUnifiedTopology: true,
+            })
+            .then(() => {
+                api = app.listen(5050)
+            })
+    });
+
+    afterAll((done) => {
+        console.log('stopping server');
+        api.close(done);
+    })
+
+    it('It retrieves the welcome message', async () => {
+        const response = await supertest(api).get('/')
+            expect(response.statusCode).toBe(200)
+    });
+
+
+
+
+})
